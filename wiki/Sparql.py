835fa9fa1f6f0c786eb66fce3ad1992642eb56de
EliasMoussamih <eliasproffessionnal@gmail.com>
2024-05-22 10:57:15 +0200

    Ajout de l'ajout des relations par l'utilisateur

--- wiki/Sparql.py
+++ wiki/Sparql.py
@@ -3,13 +3,20 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 # Définir l'endpoint SPARQL (par exemple, Wikidata)
 sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
 
+# Définir les relations dont on veut savoir le nombre d'entités communs
+relation1 = input("Quelle est votre première relation (les relations sont entre P6 et P12615) \n"+
+                  "Il peut y avoir des numéros qui ne sont liés à aucune relation \n"+
+                  "Attention à mettre le P en majuscule \n")
+relation2 = input("Quelle est votre deuxième relation \n")
+
+
 # Définir la requête SPARQL
-query = """
+query = f"""
 SELECT (COUNT(distinct ?x) AS ?count)
-WHERE {
-  ?x wdt:P31 ?y.
-  ?x wdt:P6 ?z.
-}
+WHERE {{
+  ?x wdt:{relation1} ?y.       
+  ?x wdt:{relation2} ?z.
+}}
 """
 
 # Configurer l'objet SPARQLWrapper
